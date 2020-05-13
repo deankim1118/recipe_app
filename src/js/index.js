eda3d05fff9e94a73df9bf94d4b76bcdb129ade1
deankim1118 <deankim1118@gmail.com>
2020-05-13 13:45:22 -0400

    #1.webpack.config.js
    Webpack and Babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 // Global app controller
 import test from './test';
-console.log(`I imported ${test} from another module`);
+
+const x = 1118;
+console.log(`I imported ${test} from another module and ${x}`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ['babel-polyfill', './src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: 'dist',
     compress: true,
   },
   plugins: [
@@ -17,4 +17,15 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
