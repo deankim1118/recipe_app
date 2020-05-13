c1da3a3c3abf444ffcd865c28fe90ff6a9e35184
deankim1118 <deankim1118@gmail.com>
2020-05-13 15:54:28 -0400

    #4.Global state object

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,28 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
+/** Global state of the app
+ * - Search object
+ * - Curret recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-search.getResults();
+const controlSearch = async (e) => {
+  e.preventDefault();
+  //1. Get query from the view
+  const query = 'pizza';
+
+  if (query) {
+    //2. New search object and add to state
+    state.search = new Search(query);
+
+    //3. Prepare UI for result
+    //4. Get the recipes
+    await state.search.getResults();
+    //5. Render results on UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', controlSearch);
